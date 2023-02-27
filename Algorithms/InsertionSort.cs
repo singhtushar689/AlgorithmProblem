7c47ff8097b25dc7e1d38ea5aeecb7c524b3cdfd
singhtushar689 <Singhtushar689@gmail.com>
2023-02-27 22:58:41 +0530

    [Tushar] add; Insertion Sorting In an array

---
+++ Algorithms/InsertionSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class InsertionSort
+    {
+        public void Insertion()
+        {
+            int[] arr = { 34, 5, 6, 44, 66, 74, 89, 51 };
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (arr[j].CompareTo(arr[j - 1]) < 0)
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j - 1];
+                        arr[j - 1] = temp;
+                    }
+                }
+            }
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Algorithm Programs");
+            InsertionSort sort = new InsertionSort();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\n Enter the Option GivenBelow: \n 1.InsertionSort \n 2.Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        sort.Insertion();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
+
